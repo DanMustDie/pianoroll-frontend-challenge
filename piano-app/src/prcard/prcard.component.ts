c759e296a235213d387ba1520803ba166835d496
Daniil <doritosbag8@gmail.com>
2023-11-04 12:08:49 +0100

    loads empty svgs

--- piano-app/src/prcard/prcard.component.ts
+++ piano-app/src/prcard/prcard.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from "@angular/core";
+import { Component,Input, OnInit } from "@angular/core";
 import { PianoRoll } from "src/pianoroll/pianoroll";
 import { CommonModule } from "@angular/common";
 
@@ -8,6 +8,16 @@ import { CommonModule } from "@angular/common";
     styleUrls:['./prcard.component.css']
 })
 
-export class PRCardComponent{
+export class PRCardComponent implements OnInit{
     @Input() pianoroll!: PianoRoll;
+    ngOnInit(): void {
+        const cardDiv = document.getElementsByClassName(`piano-roll-card`)[this.pianoroll.id];
+        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.classList.add('piano-roll-svg');
+        svg.setAttribute('width', '80%');
+        svg.setAttribute('height', '150');
+        if(cardDiv != null){
+            cardDiv.appendChild(svg);
+        }
+    }
 }
