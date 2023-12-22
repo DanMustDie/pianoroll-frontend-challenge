51bf50c3e9c6ef09a695d2c66459a1ad6c67cce7
Daniil <doritosbag8@gmail.com>
2023-12-22 15:07:36 +0100

    small changes

--- piano-app/src/modal/modal.component.ts
+++ piano-app/src/modal/modal.component.ts
@@ -24,14 +24,25 @@ export class ModalComponent implements OnInit{
             svg_container.appendChild(this.pianoroll.svg)
         }
         const pr_svg = document.getElementById('piano-roll-svg' + this.pianoroll.id)
+    
         
         console.log(pr_svg)
         console.log(svg_follower)
         if(pr_svg != null && svg_follower != null){
-            pr_svg.addEventListener('mousemove',(event:any) => {
+            const handleMouseMove = (event:any) => {
                 this.mousePosition = Math.floor(event.clientX - pr_svg.getBoundingClientRect().left)
                 svg_follower.style.transform = `translate(${this.mousePosition}px,0px)`
-            })
+            }
+
+            pr_svg.addEventListener('mousemove',handleMouseMove)
+            pr_svg.addEventListener('click',(event:any) => {
+                this.mousePosition = Math.floor(event.clientX - pr_svg.getBoundingClientRect().left)
+                svg_follower.style.transform = `translate(${this.mousePosition}px,0px)`
+                pr_svg.removeEventListener('mousemove',handleMouseMove)
+            },{once:true})
         }
+        //Some notes on what to do
+        //Transform all of the code of the 'follower' element as a seperate angular element for easier logic 
+        //Create a rectangle between those two elements to highlight the chosen segment 
     }
 }
